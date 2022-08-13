7e317f34499027013b385398ee5cb4bab697df3f
SainiBhavesh <bhaveshsaini2002@gmail.com>
2022-08-14 03:28:05 +0530

    Sign In functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from webbrowser import get
 from fastapi import FastAPI
 from mapwithlist import generate_mapwithlist
+import pymongo
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
@@ -9,4 +10,33 @@ def generate():
     generate_mapwithlist()
     return ""
 
+@app.get('/register/name={name}&email={email}&password={password}')
+def register(name:str,email:str,password:str):
+    client = pymongo.MongoClient("mongodb+srv://hospital:n%40GAmXWZbPr.n3c@ambulance.ymqigbw.mongodb.net/test")
+    mydb = client["ambulance"]
+    mycol = mydb["userlogin"]
+    data = {"Name":f"{name}","Email":f"{email}","Password":f"{password}"}
+    if mycol.insert_one(data):
+        return True
+    else:
+        return False
+
+@app.get('/login/name={name}&password={password}')
+def login(name:str,password:str):
+    client = pymongo.MongoClient("mongodb+srv://hospital:n%40GAmXWZbPr.n3c@ambulance.ymqigbw.mongodb.net/test")
+    mydb = client["ambulance"]
+    mycol = mydb["userlogin"]
+    if mycol.find({},{'Name':f"{name}","Password":f"{password}"}):
+        return True
+    else:
+        return False
     
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
